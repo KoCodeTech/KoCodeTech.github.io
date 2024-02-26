07df1c8b2da500807e687d83cb69605140382615
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2024-02-26 16:41:37 -0300

    Actualización de funciones y del menú

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //PRODUCTOS
 const productos = [
-    //Abrigos
+    //Promociones
     {
-        id: "abrigo-01",
-        titulo: "Desayuno 01",
+        id: "promocion-01",
+        titulo: "Café con leche, Té o chocolate + Jugo natural + Soda + 2 Facturas o Raspaditas",
         imagen: "./img/abrigos/01.jpg",
         descripcion: [
                       "2 tortitas",
@@ -11,14 +11,44 @@ const productos = [
                       "1 chocolate"
                     ],
         categoria:{
-            nombre: "Desayunos",
+            nombre: "Promos",
+            id: "promociones"
+        },
+        precio: 1800
+    },
+    {
+        id: "promocion-02",
+        titulo: "Super promo",
+        imagen: "./img/abrigos/01.jpg",
+        descripcion: [
+                      "Café con leche, Té o chocolate + Jugo natural + Soda + 2 Facturas o Raspaditas"
+                    ],
+        categoria:{
+            nombre: "Promos",
+            id: "promociones"
+        },
+        precio: 1800
+    },
+
+    //Café
+    {
+        id: "abrigo-01",
+        titulo: "Café pocillo/cortado",
+        imagen: "./img/abrigos/01.jpg",
+        descripcion: [
+                      "2 tortitas",
+                      "1 cafe",
+                      "1 chocolate"
+                    ],
+        categoria:{
+            nombre: "Cafeteria",
             id: "abrigos"
         },
-        precio: 1000
+        precio: 900
     },
     {
         id: "abrigo-02",
-        titulo: "Desayuno 02",
+        titulo: "Café jarrijo/cortado",
         imagen: "./img/abrigos/02.jpg",
         descripcion: [
             "4 tortitas",
@@ -26,14 +56,14 @@ const productos = [
             "8 chocolate"
           ],
         categoria:{
-            nombre: "Desayunos",
+            nombre: "Cafeteria",
             id: "abrigos"
         },
-        precio: 1000
+        precio: 1100
     },
     {
         id: "abrigo-03",
-        titulo: "Desayuno 03",
+        titulo: "Café doble/con leche",
         imagen: "./img/abrigos/03.jpg",
         descripcion: [
             "2 tortitas",
@@ -41,17 +71,62 @@ const productos = [
             "1 chocolate"
           ],
         categoria:{
-            nombre: "Desayunos",
+            nombre: "Cafeteria",
             id: "abrigos"
         },
-        precio: 1000
+        precio: 1500
+    },
+    {
+        id: "abrigo-04",
+        titulo: "Capuchino",
+        imagen: "./img/abrigos/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Cafeteria",
+            id: "abrigos"
+        },
+        precio: 1700
+    },
+    {
+        id: "abrigo-05",
+        titulo: "Submarino",
+        imagen: "./img/abrigos/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Cafeteria",
+            id: "abrigos"
+        },
+        precio: 1700
+    },
+    {
+        id: "abrigo-06",
+        titulo: "Té distintas variedades",
+        imagen: "./img/abrigos/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Cafeteria",
+            id: "abrigos"
+        },
+        precio: 750
     },
 
-    //Camisetas
+    //Bebidas
 
     {
         id: "camiseta-01",
-        titulo: "Bebidas 01",
+        titulo: "Gaseosas",
         imagen: "./img/camisetas/01.jpg",
         descripcion: [
             "2 tortitas",
@@ -62,11 +137,11 @@ const productos = [
             nombre: "Bebidas",
             id: "camisetas"
         },
-        precio: 1000
+        precio: 1500
     },
     {
         id: "camiseta-02",
-        titulo: "Bebidas 02",
+        titulo: "Aguas saborizadas",
         imagen: "./img/camisetas/02.jpg",
         descripcion: [
             "2 tortitas",
@@ -81,7 +156,7 @@ const productos = [
     },
     {
         id: "camiseta-03",
-        titulo: "Bebidas 03",
+        titulo: "Agua con o sin gas",
         imagen: "./img/camisetas/03.jpg",
         descripcion: [
             "2 tortitas",
@@ -92,14 +167,44 @@ const productos = [
             nombre: "Bebidas",
             id: "camisetas"
         },
-        precio: 1000
+        precio: 800
+    },
+    {
+        id: "camiseta-04",
+        titulo: "Licuados de 330cc",
+        imagen: "./img/camisetas/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Bebidas",
+            id: "camisetas"
+        },
+        precio: 2300
+    },
+    {
+        id: "camiseta-05",
+        titulo: "Jugo de naranja de 330cc",
+        imagen: "./img/camisetas/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Bebidas",
+            id: "camisetas"
+        },
+        precio: 2400
     },
 
-    //Pantalones
+    //Facturas
 
     {
         id: "pantalon-01",
-        titulo: "Facturas 01",
+        titulo: "Facturas",
         imagen: "./img/pantalones/01.jpg",
         descripcion: [
             "2 tortitas",
@@ -110,11 +215,11 @@ const productos = [
             nombre: "Facturas",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 400
     },
     {
         id: "pantalon-02",
-        titulo: "Facturas 02",
+        titulo: "Raspaditas",
         imagen: "./img/pantalones/02.jpg",
         descripcion: [
             "2 tortitas",
@@ -125,11 +230,11 @@ const productos = [
             nombre: "Facturas",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 270
     },
     {
         id: "pantalon-03",
-        titulo: "Facturas 03",
+        titulo: "Tostados",
         imagen: "./img/pantalones/03.jpg",
         descripcion: [
             "2 tortitas",
@@ -140,7 +245,67 @@ const productos = [
             nombre: "Facturas",
             id: "pantalones"
         },
-        precio: 1000
+        precio: 2700
+    },
+    {
+        id: "pantalon-04",
+        titulo: "Tostadas x3",
+        imagen: "./img/pantalones/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Facturas",
+            id: "pantalones"
+        },
+        precio: 200
+    },
+    {
+        id: "pantalon-05",
+        titulo: "Untables x2",
+        imagen: "./img/pantalones/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Facturas",
+            id: "pantalones"
+        },
+        precio: 540
+    },
+    {
+        id: "pantalon-06",
+        titulo: "Media luna con Jamón y queso",
+        imagen: "./img/pantalones/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Facturas",
+            id: "pantalones"
+        },
+        precio: 1400
+    },
+    {
+        id: "pantalon-07",
+        titulo: "Criollo con jámon y queso",
+        imagen: "./img/pantalones/03.jpg",
+        descripcion: [
+            "2 tortitas",
+            "1 cafe",
+            "1 chocolate"
+          ],
+        categoria:{
+            nombre: "Facturas",
+            id: "pantalones"
+        },
+        precio: 1200
     },
 ];
 
@@ -160,12 +325,17 @@ function cargarProductos(productosElegidos) {
         div.innerHTML = `
             <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
             <div class="producto-detalles">
-                <h3 class="producto-titulo">${producto.titulo}</h3>
-                <p class="producto-precio">$${producto.precio}</p>
-
-                <button class="producto-ver btn" onclick="toggleText('${producto.id}')">Detalles</button>
-                <div id="${producto.id}" class="descripcion">
-                    ${producto.descripcion.map(parrafo => `<p>${parrafo}</p>`).join('')}
+                <div class="producto-etiquetas">
+                    <h3 class="producto-titulo">${producto.titulo}</h3>
+                    <p class="producto-precio">$${producto.precio}</p>
+                </div>
+                
+                <div class="boton-detalles">
+                    <button class="producto-ver btn" onclick="toggleText('${producto.id}')" ontouchstart="toggleText('${producto.id}')">Detalles</button>
+                
+                    <div id="${producto.id}" class="descripcion">
+                        ${producto.descripcion.map(parrafo => `<p>${parrafo}</p>`).join('')}
+                    </div>
                 </div>
             </div>
         `;
@@ -198,9 +368,41 @@ botonesCategorias.forEach(boton =>{
 
 function toggleText(id) {
     var descripcion = document.getElementById(id);
-    if (descripcion.style.display === "none") {
+
+    if (descripcion.style.display === "none" || descripcion.style.display === "") {
       descripcion.style.display = "block";
     } else {
       descripcion.style.display = "none";
     }
-  }
+}
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe
+  
+    // Obtiene los valores de usuario y contraseña
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    // Aquí iría la lógica de autenticación, por ejemplo:
+    if (username === 'usuario' && password === 'contraseña') {
+      alert('¡Inicio de sesión exitoso!');
+      // Aquí podrías redirigir a otra página o realizar otras acciones después del inicio de sesión exitoso
+    } else {
+      document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos';
+    }
+});
+  
+// Obtenemos referencias a los elementos del DOM
+const btnMostrarVentana = document.getElementById('mostrarVentana');
+const btnCerrarVentana = document.getElementById('cerrarVentana');
+const ventanaEmergente = document.getElementById('ventanaEmergente');
+
+// Agregamos un event listener al botón para mostrar la ventana emergente
+btnMostrarVentana.addEventListener('click', function() {
+  ventanaEmergente.style.display = 'block'; // Mostramos la ventana emergente al hacer clic en el botón
+});
+
+// Agregamos un event listener al botón de cerrar para ocultar la ventana emergente
+btnCerrarVentana.addEventListener('click', function() {
+  ventanaEmergente.style.display = 'none'; // Ocultamos la ventana emergente al hacer clic en el botón de cerrar
+});
