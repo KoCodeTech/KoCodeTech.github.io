84dc591189b3dcb53ec123ee9ee511eb139aa8b3
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2024-02-29 17:09:36 -0300

    Version 1.0
    Últimos cambios en diseño y funciones

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const productos = [
     {
         id: "promocion-01",
         titulo: "Super promo 1",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "",
         descripcion: [
                       "Café con leche, Té o chocolate + Jugo natural + Soda + 2 Facturas o Raspaditas"
                     ],
@@ -17,7 +17,7 @@ const productos = [
     {
         id: "promocion-02",
         titulo: "Super promo 2",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "",
         descripcion: [
                       "Café con leche, Té o Chocolate + Soda + Porción de Macedonia"
                     ],
@@ -30,7 +30,7 @@ const productos = [
     {
         id: "promocion-03",
         titulo: "Super promo 3",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "",
         descripcion: [
                       "Café con leche, Té o Chocolate + Soda + Tostados de Jamón y queso o primavera"
                     ],
@@ -46,9 +46,9 @@ const productos = [
     {
         id: "cafe-01",
         titulo: "Café pocillo/cortado",
-        imagen: "./img/abrigos/01.jpg",
+        imagen: "",
         descripcion: [
-            "Café espresso con una pequeña cantidad de leche"
+            ""
             ],
         categoria:{
             nombre: "Cafeteria",
@@ -59,7 +59,7 @@ const productos = [
     {
         id: "cafe-02",
         titulo: "Café jarrijo/cortado",
-        imagen: "./img/abrigos/02.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -72,7 +72,7 @@ const productos = [
     {
         id: "cafe-03",
         titulo: "Café doble/con leche",
-        imagen: "./img/abrigos/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -85,9 +85,9 @@ const productos = [
     {
         id: "cafe-04",
         titulo: "Capuchino",
-        imagen: "./img/abrigos/03.jpg",
+        imagen: "",
         descripcion: [
-            "Café expreso y leche montada con vapor para darle cremosidad"
+            ""
           ],
         categoria:{
             nombre: "Cafeteria",
@@ -98,7 +98,7 @@ const productos = [
     {
         id: "cafe-05",
         titulo: "Submarino",
-        imagen: "./img/abrigos/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -111,7 +111,7 @@ const productos = [
     {
         id: "cafe-06",
         titulo: "Té distintas variedades",
-        imagen: "./img/abrigos/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -127,11 +127,9 @@ const productos = [
     {
         id: "bebida-01",
         titulo: "Gaseosas",
-        imagen: "./img/camisetas/01.jpg",
+        imagen: "",
         descripcion: [
-            "CocaCola (Original | Light | Zero)",
-            "Sprite",
-            "Fanta"
+            ""
           ],
         categoria:{
             nombre: "Bebidas",
@@ -142,9 +140,9 @@ const productos = [
     {
         id: "bebida-02",
         titulo: "Aguas saborizadas",
-        imagen: "./img/camisetas/02.jpg",
+        imagen: "",
         descripcion: [
-            "Sabores: Pomelo, Manzana, Naranja"
+            ""
           ],
         categoria:{
             nombre: "Bebidas",
@@ -155,7 +153,7 @@ const productos = [
     {
         id: "bebida-03",
         titulo: "Agua con o sin gas",
-        imagen: "./img/camisetas/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -168,7 +166,7 @@ const productos = [
     {
         id: "bebida-04",
         titulo: "Licuados de 330cc",
-        imagen: "./img/camisetas/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -181,7 +179,7 @@ const productos = [
     {
         id: "bebida-05",
         titulo: "Jugo de naranja de 330cc",
-        imagen: "./img/camisetas/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -197,7 +195,7 @@ const productos = [
     {
         id: "factura-01",
         titulo: "Facturas",
-        imagen: "./img/pantalones/01.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -210,7 +208,7 @@ const productos = [
     {
         id: "factura-02",
         titulo: "Raspaditas",
-        imagen: "./img/pantalones/02.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -223,7 +221,7 @@ const productos = [
     {
         id: "factura-03",
         titulo: "Tostados",
-        imagen: "./img/pantalones/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -236,7 +234,7 @@ const productos = [
     {
         id: "factura-04",
         titulo: "Tostadas x3",
-        imagen: "./img/pantalones/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -249,7 +247,7 @@ const productos = [
     {
         id: "factura-05",
         titulo: "Untables x2",
-        imagen: "./img/pantalones/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -262,7 +260,7 @@ const productos = [
     {
         id: "factura-06",
         titulo: "Media luna con Jamón y queso",
-        imagen: "./img/pantalones/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -275,7 +273,7 @@ const productos = [
     {
         id: "factura-07",
         titulo: "Criollo con jámon y queso",
-        imagen: "./img/pantalones/03.jpg",
+        imagen: "",
         descripcion: [
             ""
           ],
@@ -301,23 +299,22 @@ function cargarProductos(productosElegidos) {
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
-            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
             <div class="producto-detalles">
                 <div class="producto-etiquetas">
                     <h3 class="producto-titulo">${producto.titulo}</h3>
                     <p class="producto-precio">$${producto.precio}</p>
                 </div>
                 
-                <div class="boton-detalles">
-                    <button id="miBoton" class="producto-ver btn" onclick="toggleText('${producto.id}')" ontouchend="toggleText('${producto.id}')">Detalles</button>
                 
-                    <div id="${producto.id}" class="descripcion">
+                <div class="boton-detalles">
+                    ${producto.descripcion[0] !== "" ? `<button id="${producto.id}-btn" class="producto-ver btn" onclick="toggleText('${producto.id}')" ontouchend="toggleText('${producto.id}')">Detalles</button>` : ""}
+                
+                    <div id="${producto.id}" class="descripcion" style="${producto.descripcion[0] === "" ? 'display: none;' : ''}">
                         ${producto.descripcion.map(parrafo => `<p>${parrafo}</p>`).join('')}
                     </div>
                 </div>
             </div>
         `;
-
         contenedorProductos.append(div);
 
     })
@@ -353,68 +350,3 @@ function toggleText(id) {
       descripcion.style.display = "none";
     }
 }
-
-/*
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe
-  
-    // Obtiene los valores de usuario y contraseña
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-  
-    // Aquí iría la lógica de autenticación, por ejemplo:
-    if (username === 'usuario' && password === 'contraseña') {
-      alert('¡Inicio de sesión exitoso!');
-      // Aquí podrías redirigir a otra página o realizar otras acciones después del inicio de sesión exitoso
-    } else {
-      document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos';
-    }
-});
-  
-// Obtenemos referencias a los elementos del DOM
-const btnMostrarVentana = document.getElementById('mostrarVentana');
-const btnCerrarVentana = document.getElementById('cerrarVentana');
-const ventanaEmergente = document.getElementById('ventanaEmergente');
-
-// Agregamos un event listener al botón para mostrar la ventana emergente
-btnMostrarVentana.addEventListener('click', function() {
-  ventanaEmergente.style.display = 'block'; // Mostramos la ventana emergente al hacer clic en el botón
-});
-
-// Agregamos un event listener al botón de cerrar para ocultar la ventana emergente
-btnCerrarVentana.addEventListener('click', function() {
-  ventanaEmergente.style.display = 'none'; // Ocultamos la ventana emergente al hacer clic en el botón de cerrar
-});
-*/
-
-
-// Obtener el elemento del botón
-var boton = document.getElementById('miBoton');
-
-// Bandera para indicar si se está deslizando
-var deslizando = false;
-
-// Manejador de evento para el inicio del deslizamiento
-function inicioDeslizamiento() {
-    deslizando = true;
-    boton.disabled = true; // Desactivar el botón durante el deslizamiento
-}
-
-// Manejador de evento para el final del deslizamiento
-function finDeslizamiento() {
-    deslizando = false;
-    boton.disabled = false; // Volver a activar el botón al final del deslizamiento
-}
-
-// Manejador de evento para toque en el botón
-function toqueEnBoton() {
-    if (!deslizando) {
-        // Si no se está deslizando, realizar la acción deseada
-        console.log('Botón tocado');
-    }
-}
-
-// Agregar oyentes de eventos
-boton.addEventListener('touchstart', inicioDeslizamiento);
-boton.addEventListener('touchend', finDeslizamiento);
-boton.addEventListener('click', toqueEnBoton);
